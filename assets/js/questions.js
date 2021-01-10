ce0e4a3670c205df414d7c4170c4960dc7251ddd
alec <alexeialvarez4@gmail.com>
2021-01-10 09:19:35 -0600

    refactor designed, and logical to in js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,57 +1,106 @@
-class GenerateQuestions {
-  #questionsName;
-  #correctAnswer;
-  #answers;
+const api = 'https://restcountries.eu/rest/v2/all';
 
-  constructor(questionName, correctAnswer, answers) {
-    this.#questionsName = questionName;
-    this.#correctAnswer = correctAnswer;
-    this.#answers = answers;
-  }
-
-  getQuestionsName() {
-    return this.#questionsName;
-  }
-  getCorrectQuestion() {
-    return this.#correctAnswer;
-  }
-  getAllAnswers() {
-    return this.#answers;
-  }
+async function getData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-const a = new GenerateQuestions('Kuala Lumpur is the capital of?', 'malaysia', [
-  'vietname',
-  'malaysia',
-  'sweden',
-  'austria',
-]);
+function getRandomCountry(countries) {
+  const random = Math.floor(Math.random() * countries.length);
+  return {
+    name: countries[random].name,
+    capital: countries[random].capital,
+  };
+}
 
-const b = new GenerateQuestions('Madrid is the capital of?', 'spain', [
-  'spain',
-  'malaysia',
-  'vietnam',
-  'washintog',
-]);
+function getRandomCountries(country, countries) {
+  const random = Math.floor(Math.random() * (countries.length - 50));
+  const randomCountries = countries.slice(random, random + 3);
+  return randomCountries.filter(el => {
+    return el.name !== country.name;
+  });
+}
+function moveRandomOptions(arr) {
+  let currentIdx = arr.length,
+    tempValue,
+    randomIdx;
 
-const c = new GenerateQuestions('berlin is the capital of?', 'germany', [
-  'germany',
-  'spain',
-  'rusia',
-  'england',
-]);
+  while (0 !== +currentIdx) {
+    randomIdx = Math.floor(Math.random() * currentIdx);
+    currentIdx -= 1;
 
-const d = new GenerateQuestions(
-  'washington is the capital of?',
-  'united states',
-  ['berlin', 'malaysia', 'vietnam', 'united states']
-);
+    tempValue = arr[currentIdx];
+    arr[currentIdx] = arr[randomIdx];
+    arr[randomIdx] = tempValue;
+  }
+  return arr;
+}
 
-const e = new GenerateQuestions('Sidney is the capital of?', 'australia', [
-  'kawai',
-  'australia',
-  'united states',
-  'germany',
-]);
+async function buildQuestion() {
+  try {
+    const countries = await getData(api);
+    const country = getRandomCountry(countries);
 
-export const countriesQuestions = [a, b, c, d, e];
+    const quizz = {
+      question: `${country.capital} is capital of`,
+      correctAnswer: country.name,
+      options: moveRandomOptions([
+        country,
+        ...getRandomCountries(country, countries),
+      ]),
+    };
+    console.log(quizz);
+  } catch (err) {
+    console.warn(err);
+  }
+}
+buildQuestion();
+buildQuestion();
+buildQuestion();
+buildQuestion();
+buildQuestion();
+
+// async function cleanData() {
+//   const data = await getData(api);
+//   const random = Math.floor(Math.random() * 200 + 0);
+//   const countries = data.slice(random, random + 5);
+//   return countries;
+// }
+
+// async function buildQuestions() {
+//   const countries = await cleanData();
+
+//   const questions = countries.map((country, index) => {
+//     const orderedOptions = countries.map(c => {
+//       if (c.name === countries[index].name) {
+//         return {
+//           capital: country.capital,
+//           isCorrect: true,
+//         };
+//       } else {
+//         return {
+//           capital: c.capital,
+//           isCorrect: false,
+//         };
+//       }
+//     });
+//     const random = Math.floor(Math.random() * countries.length + 0);
+//     const options = moveRandomOptions(orderedOptions, 0, random);
+
+//     return {
+//       name: country.name,
+//       options,
+//     };
+//   });
+//   return questions;
+// }
+
+// function moveRandomOptions(arr, fromIndex, toIndex) {
+//   const element = arr[fromIndex];
+//   arr.splice(fromIndex, 1);
+//   arr.splice(toIndex, 0, element);
+//   return arr;
+// }
+
+// buildQuestions();
